b6b63d7d75d1a1c17856f4c555cddad7a9db2e7f
sanchitaithub12 <sanchitagithub@gmail.com>
2021-08-15 21:40:40 +0530

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      In this Vue 3 tutorial we'll take a look at the Vue Router in order to
+      make a "multi-page" Vue website. We'll see how to set uo the Vue Router,
+      how to use Router Links, pass route parameters & more.
+    </p>
+    <p>
+      In this Vue 3 tutorial we'll take a look at the Vue Router in order to
+      make a "multi-page" Vue website. We'll see how to set uo the Vue Router,
+      how to use Router Links, pass route parameters & more.
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>
+      In this Vue 3 tutorial we'll take a look at the Vue Router in order to
+      make a "multi-page" Vue website. We'll see how to set uo the Vue Router,
+      how to use Router Links, pass route parameters & more.
+    </p>
+    <p>
+      In this Vue 3 tutorial we'll take a look at the Vue Router in order to
+      make a "multi-page" Vue website. We'll see how to set uo the Vue Router,
+      how to use Router Links, pass route parameters & more.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
